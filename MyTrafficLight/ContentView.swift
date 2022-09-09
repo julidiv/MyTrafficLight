29a91f1eb75abe09d3731325c7b7439b3448c9c2
julidiv <julia.dzivina@gmail.com>
2022-09-09 16:28:49 +0200

    Initial Commit

---
+++ MyTrafficLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTrafficLight
+//
+//  Created by Alexander Golub on 9.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyTrafficLight/MyTrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTrafficLightApp.swift
+//  MyTrafficLight
+//
+//  Created by Alexander Golub on 9.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MyTrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
